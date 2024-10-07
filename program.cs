7c78419302d0dbfdc2f040fcc627eb7715e63ceb
Daniil <65912505+danbruh228@users.noreply.github.com>
2024-10-08 01:51:24 +0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,13 @@
+Console.Write("bool = "); Console.Write(bool.TrueString); Console.Write("/"); Console.WriteLine(bool.FalseString);
+Console.Write(byte.MinValue); Console.Write(" <= byte <= "); Console.WriteLine(byte.MaxValue);
+Console.Write(sbyte.MinValue); Console.Write(" <= sbyte <= "); Console.WriteLine(sbyte.MaxValue);
+Console.Write(short.MinValue); Console.Write(" <= short <= "); Console.WriteLine(short.MaxValue);
+Console.Write(ushort.MinValue); Console.Write(" <= ushort <= "); Console.WriteLine(ushort.MaxValue);
+Console.Write(int.MinValue); Console.Write(" <= int <= "); Console.WriteLine(int.MaxValue);
+Console.Write(uint.MinValue); Console.Write(" <= uint <= "); Console.WriteLine(uint.MaxValue);
+Console.Write(long.MinValue); Console.Write(" <= long <= "); Console.WriteLine(long.MaxValue);
+Console.Write(ulong.MinValue); Console.Write(" <= ulong <= "); Console.WriteLine(ulong.MaxValue);
+Console.Write(float.MinValue); Console.Write(" <= float <= "); Console.WriteLine(float.MaxValue);
+Console.Write(double.MinValue); Console.Write(" <= double <= "); Console.WriteLine(double.MaxValue);
+Console.Write(decimal.MinValue); Console.Write(" <= decimal <= "); Console.WriteLine(decimal.MaxValue);
+Console.Write(char.MinValue); Console.Write(" <= char <= "); Console.WriteLine(char.MaxValue);
